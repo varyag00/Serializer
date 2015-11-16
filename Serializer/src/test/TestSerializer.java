496ca17bb23d58be20027618d10b8c2d1e3f5dd4
Dan Gonzalez <d.gonzalez0902@gmail.com>
2015-11-16 03:20:40 -0700

    Added test for Serializer

--- Serializer/src/test/TestSerializer.java
+++ Serializer/src/test/TestSerializer.java
@@ -2,6 +2,8 @@ package test;
 
 import static org.junit.Assert.*;
 
+import org.jdom2.Document;
+import org.jdom2.output.XMLOutputter;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -24,10 +26,20 @@ public class TestSerializer {
 
 	@After
 	public void tearDown() throws Exception {
+		objCreator = null;
+		o = null;
+		ser = null;
 	}
 
 	@Test
-	public void test() {
+	public void testSerialize() {
+		String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<serialized><object class=\"objects.Object0\" id=\"0\"><field name=\"int0\" declaringClass=\"objects.Object0\"><value>0</value></field><field name=\"int1\" declaringClass=\"objects.Object0\"><value>0</value></field><field name=\"doub42\" declaringClass=\"objects.Object0\"><value>42.0</value></field></object></serialized>\n";
+		Document doc = ser.serialize(o);
+		
+		XMLOutputter outputter = new XMLOutputter();
+		String actual = outputter.outputString(doc);
+		
+		assertEquals(expected, actual);
 	}
 
 }
